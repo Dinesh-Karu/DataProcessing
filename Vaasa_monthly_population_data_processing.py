2aa2f5d829133628b8fb9f68a2eb108c2a86a0d5
Dinesh Karunarathne <din.karu.fi@gmail.com>
2025-03-26 19:24:56 +0200

    Update Vaasa_monthly_population_data_processing.py

--- Vaasa_monthly_population_data_processing.py
+++ Vaasa_monthly_population_data_processing.py
@@ -6,7 +6,7 @@ output_file = "C:/_VAMK/_Coding/Data/Output/Population by year Vaasa.csv.csv"
 df = pd.read_csv(input_file, sep=",")
 output_data = []
 prev_year = '0'
-annual_population = 0
+total = 0
 
 # Process each row in input data
 for _, row in df.iterrows():
@@ -16,15 +16,15 @@ for _, row in df.iterrows():
     year = row["Year_Month"].split("M")[0]
     if prev_year != year:
         if prev_year != '0':
-            output_data.append([year, annual_population])
-        annual_population = population
+            output_data.append([year, round(total/12)])
+        total = population
     else:
-        annual_population += population
+        total += population
 
     prev_year = year
 
 # Convert to DataFrame
-output_df = pd.DataFrame(output_data, columns=["Year", "Annual_Population"])
+output_df = pd.DataFrame(output_data, columns=["Year", "Avg_Population"])
 # Save to output file
 output_df.to_csv(output_file, sep=",", index=False)
 
