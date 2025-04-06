1b25acb8da70b9b558333478426cfd06a9c6535b
Dinesh Karunarathne <din.karu.fi@gmail.com>
2025-04-06 16:59:39 +0300

    Update sun_rise_set_data_processing.py

--- sun_rise_set_data_processing.py
+++ sun_rise_set_data_processing.py
@@ -1,7 +1,8 @@
 import pandas as pd
+from datetime import date
 
-input_file = "C:/_VAMK/_Coding/Data/Input/input.csv"
-output_file = "C:/_VAMK/_Coding/Data/Output/output.csv"
+input_file = "C:/_VAMK/_Coding/Data/Input/Daily Sun in Vaasa 2024.csv"
+output_file = "C:/_VAMK/_Coding/Data/Output/Hourly Sun in Vaasa 2024.csv"
 
 df = pd.read_csv(input_file, sep=",")
 
@@ -12,28 +13,29 @@ for _, row in df.iterrows():
     year = row["Year"]
     month = row["Month"]
     day = row["Day"]
+    isoweekday = date(int(year), int(month), int(day)).isoweekday()
 
     begin_hour = int(row["Sunrise"].split(":")[0])
-    if int(row["Sunrise"].split(":")[1]) > 30:
+    if int(row["Sunrise"].split(":")[1]) > 30:  ##if minutes part is more than 30
         begin_hour += 1
-    end_hour = int(row["Sunset"].split(":")[0])
-    if int(row["Sunset"].split(":")[1]) > 30:
+    end_hour = int(row["Sunset"].split(":")[0]) 
+    if int(row["Sunset"].split(":")[1]) > 30:   ##if minutes part is more than 30
         end_hour += 1
     
     # Create 24-hour flag data
-    for hour in range(1, 25):
+    for hour in range(0, 24):
         if begin_hour <= hour <= begin_hour + 1:
-            flag = 2 
+            flag = 1 
         elif begin_hour + 1 < hour < end_hour - 1:
-            flag = 3
-        elif end_hour - 1 <= hour <= end_hour:
             flag = 2
-        else:
+        elif end_hour - 1 <= hour <= end_hour:
             flag = 1
-        output_data.append([year, month, day, hour, flag])
+        else:
+            flag = 0
+        output_data.append([year, month, day, isoweekday, hour, flag])
 
 # Convert to DataFrame
-output_df = pd.DataFrame(output_data, columns=["Year", "Month", "Day", "Hour", "Flag"])
+output_df = pd.DataFrame(output_data, columns=["Year", "Month", "Day", "Iso_Weekday", "Hour", "Sun_Flag"])
 # Save to output file
 output_df.to_csv(output_file, sep=",", index=False)
 
