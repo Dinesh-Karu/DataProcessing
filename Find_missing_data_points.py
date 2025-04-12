6010751beab85b08b5001d6b573513fc872400ec
Dinesh Karunarathne <din.karu.fi@gmail.com>
2025-04-12 15:37:20 +0300

    Update Find_missing_data_points.py

--- Find_missing_data_points.py
+++ Find_missing_data_points.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
 ref_file = "C:/_VAMK/_Coding/Data/Input/Ref Hourly Data Range 2020-2024.csv"
-input_file = "C:/_VAMK/_Coding/Data/Input/Hourly Average temperature Vaasa 2020-2024.csv"
+input_file = "C:/_VAMK/_Coding/Data/Input/Hourly Average Temperature Vaasa 2020-2024.csv"
 output_file = "C:/_VAMK/_Coding/Data/Output/Missing data points marked Hourly Avg Temp Vaasa 2020-2024.csv"
+log_file = "C:/_VAMK/_Coding/Data/Output/Log Missing data points of Hourly Avg Temp Vaasa 2020-2024.csv"
 
 df = pd.read_csv(input_file, sep=",")
 # Convert the DataFrame to a Dictionary
@@ -10,6 +11,7 @@ input_data_dict = df.to_dict(orient='records')
 
 df = pd.read_csv(ref_file, sep=",")
 output_data = []
+log_data = []
 i = 0
 
 # Process each row in input data
@@ -34,9 +36,14 @@ for _, row in df.iterrows():
         output_data.append([ref_year, ref_month, ref_day, ref_hour, 
                             "missing", 
                             "missing"])
+        log_data.append([ref_year, ref_month, ref_day, ref_hour])
 
 # Convert to DataFrame and Save to output file
 output_df = pd.DataFrame(output_data, columns=["Year", "Month", "Day", "Hour", "Avg_temperature", "Observation_Station"])
 output_df.to_csv(output_file, sep=",", index=False)
+output_df = pd.DataFrame(log_data, columns=["Year", "Month", "Day", "Hour"])
+output_df.to_csv(log_file, sep=",", index=False)
 
-print(f"Processed data saved to {output_file}")
+print(f"Processed data saved to")
+print(f"1. {output_file}")
+print(f"2. {log_file}")
