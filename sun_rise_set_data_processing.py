956dbe78367e95d97ca69305dc863ea6e8776135
Dinesh Karunarathne <din.karu.fi@gmail.com>
2025-04-12 17:00:53 +0300

    Update sun_rise_set_data_processing.py

--- sun_rise_set_data_processing.py
+++ sun_rise_set_data_processing.py
@@ -9,6 +9,27 @@ df = pd.read_csv(input_file, sep=",")
 
 output_hourly_data = []
 output_daily_data = []
+# Dictionay with days labeled
+weekdays_ = { 1: "Mon",
+			  2: "Tue",
+		      3: "Wed",
+			  4: "Thu",
+			  5: "Fri",
+			  6: "Sat",
+			  7: "Sun"}
+# Dictionay with months labeled
+months_ = { 1: "Jan",
+            2: "Feb",
+			3: "Mar",
+			4: "Apr",
+			5: "May",
+			6: "Jun",
+			7: "Jul",
+			8: "Aug",
+			9: "Sep",
+			10: "Oct",
+			11: "Nov",
+			12: "Dec"}
 
 # Process each row in input data
 for _, row in df.iterrows():
@@ -18,7 +39,7 @@ for _, row in df.iterrows():
     duration_ = row["Length"].split(":")[0] + "." + row["Length"].split(":")[1]
     iso_weekday = date(int(year_), int(month_), int(day_)).isoweekday()
 
-    output_daily_data.append([year_, month_, day_, iso_weekday, duration_])
+    output_daily_data.append([year_, month_, months_[month_], day_, iso_weekday, weekdays_[iso_weekday], duration_])
 
     begin_hour = int(row["Sunrise"].split(":")[0])
     if int(row["Sunrise"].split(":")[1]) > 30:  ##if minutes part is more than 30
@@ -37,13 +58,13 @@ for _, row in df.iterrows():
             flag_ = 1
         else:
             flag_ = 0
-        output_hourly_data.append([year_, month_, day_, iso_weekday, hour_, flag_])
+        output_hourly_data.append([year_, month_, months_[month_], day_, iso_weekday, weekdays_[iso_weekday], hour_, flag_])
 
 # Save hourly output
-output_df = pd.DataFrame(output_hourly_data, columns=["Year", "Month", "Day", "Iso_Weekday", "Hour", "Sun_Flag"])
+output_df = pd.DataFrame(output_hourly_data, columns=["Year", "Month_No", "Month", "Day", "Iso_Weekday", "Day of the Week", "Hour", "Sun_Flag"])
 output_df.to_csv(output_hourly_file, sep=",", index=False)
 # Save daily output
-output_df = pd.DataFrame(output_daily_data, columns=["Year", "Month", "Day", "Iso_Weekday", "Duration"])
+output_df = pd.DataFrame(output_daily_data, columns=["Year", "Month_No", "Month", "Day", "Iso_Weekday", "Day of the Week", "Duration"])
 output_df.to_csv(output_daily_file, sep=",", index=False)
 
 print(f"Processed data saved to")
